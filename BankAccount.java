7287298a531763891fd0550552b56bb3c9639b2f
Brian Falasz <bfalasz@yahoo.com>
2023-05-19 22:08:55 -0500

    setup a simple calculator in Java and all tests are passing

--- BankAccount.java
+++ BankAccount.java
@@ -1,68 +1,68 @@
 public class BankAccount {
 
-  private int AccountNumber;
-  private double AccountBalance;
-  private String CustomerName;
-  private String Email;
-  private String PhoneNumber;
+  private int accountNumber;
+  private double accountBalance;
+  private String customerName;
+  private String customerEmail;
+  private String customerPhoneNumber;
 
   /* Getter Methods */
 
   public int getAccountNumber(){
-    return this.AccountNumber;
+    return this.accountNumber;
   }
   
   public double getAccountBalance(){
-    return this.AccountBalance;
+    return this.accountBalance;
   }
 
   public String getCustomerName(){
-    return this.CustomerName;
+    return this.customerName;
   }
 
   public String getEmail(){
-    return this.Email;
+    return this.customerEmail;
   }
 
   public String getPhoneNumber(){
-    return this.PhoneNumber;
+    return this.customerPhoneNumber;
   }
 
   /* Setter Methods */
 
-  public void setAccountNumber(int AccountNumber){
-    this.AccountNumber = AccountNumber;
+  public void setAccountNumber(int accountNumber){
+    this.accountNumber = accountNumber;
   }
   
-  public void setAccountBalance(double AccountBalance){
-    this.AccountBalance = AccountBalance;
+  public void setAccountBalance(double accountBalance){
+    this.accountBalance = accountBalance;
   }
 
-  public void setCustomerName(String CustomerName){
-    this.CustomerName = CustomerName;
+  public void setCustomerName(String customerName){
+    this.customerName = customerName;
   }
 
-  public void setEmail(String Email){
-    this.Email = Email;
+  public void setEmail(String customerEmail){
+    this.customerEmail = customerEmail;
   }
 
- public void setPhoneNumber(String PhoneNumber){
-    this.PhoneNumber = PhoneNumber;
+ public void setPhoneNumber(String customerPhoneNumber){
+    this.customerPhoneNumber = customerPhoneNumber;
   }
 
   /* Deposit and Withdrawal Methods */
 
   public void deposit(double DepositAmount){
 
-    this.AccountBalance = this.AccountBalance + DepositAmount;
+    this.accountBalance = accountBalance + DepositAmount;
   }
 
   public void withdraw(double WithdrawalAmount){
 
-    double newBalance = this.AccountBalance - WithdrawalAmount;
+    double newBalance = accountBalance - WithdrawalAmount;
 
     if(newBalance >= 0){
-      this.AccountBalance = newBalance;
+      this.accountBalance = newBalance;
     } else {
       System.out.println("You do not have enough funds to make the withdrawal");
     }
--- Main.java
+++ Main.java
@@ -2,26 +2,37 @@ public class Main {
 
   public static void main(String args[]){
     
-    BankAccount myBankAccount = new BankAccount();
+    // BankAccount myBankAccount = new BankAccount();
 
-    myBankAccount.setAccountNumber(1929499);
-    myBankAccount.setAccountBalance(20000.12);
-    myBankAccount.setCustomerName("Brian Falasz");
-    myBankAccount.setEmail("brian@test.com");
-    myBankAccount.setPhoneNumber("555-555-5555");
+    // myBankAccount.setAccountNumber(1929499);
+    // myBankAccount.setAccountBalance(20000.12);
+    // myBankAccount.setCustomerName("Brian Falasz");
+    // myBankAccount.setEmail("brian@test.com");
+    // myBankAccount.setPhoneNumber("555-555-5555");
 
-    System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
+    // System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
 
-    myBankAccount.deposit(4500.82);
+    // myBankAccount.deposit(4500.82);
 
-    System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
+    // System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
 
-    myBankAccount.withdraw(3499.29);
+    // myBankAccount.withdraw(3499.29);
 
-    System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
+    // System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
 
-    myBankAccount.withdraw(23499.29);
+    // myBankAccount.withdraw(23499.29);
 
-    System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
+    // System.out.println(myBankAccount.getCustomerName() + " has a bank account with a balance of " + String.format("%.02f", myBankAccount.getAccountBalance()));
+
+    SimpleCalculator calculator = new SimpleCalculator();
+
+    calculator.setFirstNumber(11);
+    calculator.setSecondNumber(33);
+
+    System.out.println(calculator.getAdditionResult());
+
+
+
+
   }
 }
