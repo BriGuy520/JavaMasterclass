956469931fc57de4a86c0e44f6ba6e1aaeb42172
Brian Falasz <bfalasz@yahoo.com>
2023-05-25 19:19:24 -0500

    adding my solution the pool area challenge

---
+++ PoolArea/Rectangle.java
@@ -0,0 +1,25 @@
+package PoolArea;
+
+public class Rectangle {
+  
+  private double length;
+  private double width;
+
+  public Rectangle(double width, double length){
+    this.width = width < 0.0 ? 0.0 : width;
+    this.length = length < 0.0 ? 0.0 : length;
+  }
+
+  public double getLength(){
+    return length;
+  }
+
+  public double getWidth(){
+    return width;
+  }
+
+  public double getArea(){
+    return width * length;
+  }
+
+}
