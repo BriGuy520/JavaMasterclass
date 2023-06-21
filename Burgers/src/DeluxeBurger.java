3bba6e16fe64a28334b791d10e16ee54ffae942b
Brian Falasz <bfalasz@yahoo.com>
2023-06-20 20:32:08 -0500

    adding more of my progress of the hamburger challenge

--- Burgers/src/DeluxeBurger.java
+++ Burgers/src/DeluxeBurger.java
@@ -4,6 +4,16 @@ public class DeluxeBurger extends Hamburger {
 
   
   public DeluxeBurger(){
-    super("Deluxe Burger", "Beef", "White", 19.10);
+    super("Deluxe Burger", "Beef", "White", 14.54);
+  }
+
+  @Override 
+  public void addHamburgerAddition1(String name, double price){
+    super.addHamburgerAddition1("Chips", 2.75);
+  }
+
+  @Override 
+  public void addHamburgerAddition2(String name, double price){
+    super.addHamburgerAddition2("Drink", 1.81);
   }
 }
