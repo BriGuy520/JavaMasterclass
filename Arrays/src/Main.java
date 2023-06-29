fbb40c30975ad46f93bcfea9832423e95f181463
Brian Falasz <bfalasz@yahoo.com>
2023-06-28 20:47:25 -0500

    reversed the array without the reverseOrder method from the Collections library

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -2,12 +2,13 @@ package Arrays.src;
 
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
   public static void main(String[] args) {
     
-    int[] arr = new int[5];
+    Integer[] arr = new Integer[5];
     Random randomInt = new Random();
     
     for(int i = 0; i < arr.length; i++){
@@ -15,10 +16,22 @@ public class Main {
       arr[i] = randomInt.nextInt(100);
     }
 
-    System.out.print(Arrays.toString(arr));
+    System.out.println(Arrays.toString(arr));
+
     Arrays.sort(arr);
-    System.out.println();
-    System.out.print(Arrays.toString(arr));
+
+    Integer[] arr2 = Arrays.copyOf(arr, arr.length);
+
+    for(int j = arr2.length - 1; j >= 0; j--){
+      arr[j] = arr2[arr2.length - 1 - j]; 
+    }
+
+    System.out.println("First Reverse Sort: ");
+    System.out.println(Arrays.toString(arr));
+
+    Arrays.sort(arr, Collections.reverseOrder());
+    System.out.println("Second Reverse Sort: ");
+    System.out.println(Arrays.toString(arr));
   }
   
 }
